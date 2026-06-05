c24d3dfca25e35192d980b77d1c800abcc39b4b2
shamil <shamil6282669@gmail.com>
2026-06-06 00:24:29 +0530

    dockrize monitoring application

--- python_monitor/service_monitor.py
+++ python_monitor/service_monitor.py
@@ -12,25 +12,31 @@ def service_check():
 
     for service in service_names:
 
-        result = subprocess.run(
-            ["systemctl", "is-active", "--quiet", service]
-        )
+        try:
 
-        if result.returncode == 0:
-            print(f"{service} service is running")
-            write_log(f"{service} service is running")
-
-            services[service] = "Running"
-
-        else:
-            print(f"WARNING: {service} service is DOWN")
-            write_log(f"WARNING: {service} service is DOWN")
-
-            services[service] = "DOWN"
-
-            send_email_alert(
-                "SERVICE ALERT",
-                f"{service} Service is Down"
+            result = subprocess.run(
+                ["systemctl", "is-active", "--quiet", service]
             )
+
+            if result.returncode == 0:
+
+                print(f"{service} service is running")
+                write_log(f"{service} service is running")
+
+                services[service] = "Running"
+
+            else:
+
+                print(f"WARNING: {service} service is DOWN")
+                write_log(f"WARNING: {service} service is DOWN")
+
+                services[service] = "DOWN"
+
+        except FileNotFoundError:
+
+            print(f"{service} : Not Available")
+            write_log(f"{service} : Not Available")
+
+            services[service] = "Not Available"
 
     return services
